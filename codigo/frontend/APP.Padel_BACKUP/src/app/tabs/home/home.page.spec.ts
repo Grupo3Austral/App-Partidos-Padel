94875cfcb606ed38dce3ddada9fc947a6fdefa9d
ramirolopezspada <rlopezspada@mail.austral.edu.ar>
2025-10-13 18:24:24 -0300

    cambios pagina

---
+++ codigo/frontend/APP.Padel_BACKUP/src/app/tabs/home/home.page.spec.ts
@@ -0,0 +1,37 @@
+<<<<<<<< HEAD:codigo/frontend/APP.Padel/src/app/tabs/home/home.page.spec.ts
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomePage } from './home.page';
+
+describe('HomePage', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+========
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomePage } from './home.page';
+
+describe('HomePage', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+>>>>>>>> 5b34561553623890e30d5941b629118f5427c67e:codigo/frontend/App Partidos Padel/src/app/tabs/home/home.page.spec.ts
