8c8cf22771e9f4f0d84b302b36dfee26a449d3f5
ramirolopezspada <rlopezspada@mail.austral.edu.ar>
2025-10-14 18:12:14 -0300

    okaaa

---
+++ codigo/frontend/APP.Padel_BACKUP/.angular/cache/20.3.2/app/vite/deps/hardware-back-button-Dhbd-23H-HBKYN6UL.js
@@ -0,0 +1,17 @@
+import {
+  MENU_BACK_BUTTON_PRIORITY,
+  OVERLAY_BACK_BUTTON_PRIORITY,
+  blockHardwareBackButton,
+  shouldUseCloseWatcher,
+  startHardwareBackButton
+} from "./chunk-Q7IJUT7D.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-VMH42X3Y.js";
+import "./chunk-QXFS4N4X.js";
+export {
+  MENU_BACK_BUTTON_PRIORITY,
+  OVERLAY_BACK_BUTTON_PRIORITY,
+  blockHardwareBackButton,
+  shouldUseCloseWatcher,
+  startHardwareBackButton
+};
