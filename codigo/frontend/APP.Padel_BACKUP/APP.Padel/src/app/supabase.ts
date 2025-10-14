597e408dfb09b20fdd32c3f009efb2913362b49a
Joaquin <joacofrias06@gmail.com>
2025-10-14 18:11:32 -0300

    cambios supabase y html

--- codigo/frontend/APP.Padel_BACKUP/APP.Padel/src/app/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://kpagqfrrcleqtkatmtda.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwYWdxZnJyY2xlcXRrYXRtdGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMjExMzMsImV4cCI6MjA3MTg5NzEzM30.tbNWaV5oTEWC1Cub-4q-0Rxv1c9g46b9YZ3cvu6r28Q';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
