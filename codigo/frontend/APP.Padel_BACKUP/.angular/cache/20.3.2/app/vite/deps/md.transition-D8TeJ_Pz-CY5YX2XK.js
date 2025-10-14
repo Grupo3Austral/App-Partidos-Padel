78f1829acf1320f72e31963d899300c5079149ff
ramirolopezspada <rlopezspada@mail.austral.edu.ar>
2025-10-14 18:12:14 -0300

    okaaa

---
+++ codigo/frontend/APP.Padel_BACKUP/.angular/cache/20.3.2/app/vite/deps/md.transition-D8TeJ_Pz-CY5YX2XK.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-CWJAVQJS.js";
+import "./chunk-JR3U6AKC.js";
+import "./chunk-M4WU4XXB.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-WGSNIJAH.js";
+import "./chunk-VMH42X3Y.js";
+import "./chunk-QXFS4N4X.js";
+export {
+  mdTransitionAnimation
+};
