e8c66cd9a6d9846f47548a3bcd802ad456067c89
Joaquin <joacofrias06@gmail.com>
2025-10-15 14:33:55 -0300

    cambios tab estadistica

--- codigo/frontend/AppPadel/src/environments/environment.ts
+++ codigo/frontend/AppPadel/src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: 'https://TU_PROYECTO.supabase.co',
-  supabaseKey: 'TU_PUBLIC_ANON_KEY'
+  supabaseUrl: 'https://kpagqfrrcleqtkatmtda.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwYWdxZnJyY2xlcXRrYXRtdGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMjExMzMsImV4cCI6MjA3MTg5NzEzM30.tbNWaV5oTEWC1Cub-4q-0Rxv1c9g46b9YZ3cvu6r28Q'
 };
 
 /*
